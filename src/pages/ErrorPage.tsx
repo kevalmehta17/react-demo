54460eb53bfa47d28b42a1a5969da5ec1093818a
keval <kevalemhta885@gmail.com>
2026-03-03 12:49:00 +0530

    updated

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      
+      <h1 style={{ color: "red" }}>404- Page Not Found</h1>
+      <hr />
+      <div>
+        <strong> Sorry! You are looking For the Page that not Exist</strong>
+        <div style={{ display: "flex", padding: "10px" }}>
+          <button onClick={() => navigate(-1)}>Go Back</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
